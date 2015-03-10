cbe4670e02b96a48a0c8c792067df934caa22028
Zach Marano <zmarano@google.com>
2015-03-09 18:28:44 -0700

    Bump version to 1.2.4

--- gcimagebundle/gcimagebundlelib/imagebundle.py
+++ gcimagebundle/gcimagebundlelib/imagebundle.py
@@ -142,7 +142,7 @@ def SetupLogging(options, log_dir='/tmp'):
 
 def PrintVersionInfo():
   #TODO: Should read from the VERSION file instead.
-  print 'version 1.2.3'
+  print 'version 1.2.4'
 
 
 def GetTargetFilesystem(options, guest_platform):
