4fef45d812aaf6c4c9d805dc15062ba13213958d
Rick Wright <rickw@google.com>
2015-08-12 13:40:57 -0700

    Update version to 1.2.8

--- gcimagebundle/gcimagebundlelib/imagebundle.py
+++ gcimagebundle/gcimagebundlelib/imagebundle.py
@@ -142,7 +142,7 @@ def SetupLogging(options, log_dir='/tmp'):
 
 def PrintVersionInfo():
   #TODO: Should read from the VERSION file instead.
-  print 'version 1.2.7'
+  print 'version 1.2.8'
 
 
 def GetTargetFilesystem(options, guest_platform):
