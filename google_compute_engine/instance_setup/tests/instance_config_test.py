c3acff687f96ad8fede90108438d882c91e36f76
Max Illfelder <illfelder@google.com>
2017-06-07 20:51:46 -0700

    Rename config metadata key and fix config logic.

--- google_compute_engine/instance_setup/tests/instance_config_test.py
+++ google_compute_engine/instance_setup/tests/instance_config_test.py
@@ -23,6 +23,7 @@ from google_compute_engine.test_compat import unittest
 class InstanceConfigTest(unittest.TestCase):
 
   def setUp(self):
+    self.mock_logger = mock.Mock()
     instance_config.InstanceConfig.instance_config = 'config'
     instance_config.InstanceConfig.instance_config_distro = 'distro'
     instance_config.InstanceConfig.instance_config_template = 'template'
@@ -50,9 +51,10 @@ class InstanceConfigTest(unittest.TestCase):
     mocks.attach_mock(mock_init, 'init')
     mocks.attach_mock(mock_set, 'set')
     mocks.attach_mock(mock_exists, 'exists')
+    mocks.attach_mock(self.mock_logger, 'logger')
     mock_exists.return_value = False
 
-    instance_config.InstanceConfig()
+    instance_config.InstanceConfig(logger=self.mock_logger)
     expected_calls = [
         mock.call.init(
             config_file='template', config_header='/tmp/test.py template'),
@@ -88,9 +90,11 @@ class InstanceConfigTest(unittest.TestCase):
       mocks.attach_mock(mock_parser, 'parser')
       mocks.attach_mock(mock_config, 'config')
       mocks.attach_mock(mock_exists, 'exists')
+      mocks.attach_mock(self.mock_logger, 'logger')
       mock_exists.return_value = True
 
-      instance_config.InstanceConfig(instance_config_metadata=config_metadata)
+      instance_config.InstanceConfig(
+          logger=self.mock_logger, instance_config_metadata=config_metadata)
       expected_calls = [
           mock.call.init(
               config_file='template', config_header='/tmp/test.py template'),
@@ -115,6 +119,40 @@ class InstanceConfigTest(unittest.TestCase):
           mock.call.set('third', 'e', '3', overwrite=False)
       ]
       self.assertEqual(mocks.mock_calls, expected_calls)
+
+  @mock.patch('google_compute_engine.instance_setup.instance_config.parser.SafeConfigParser.read')
+  @mock.patch('google_compute_engine.instance_setup.instance_config.os.path.exists')
+  @mock.patch('google_compute_engine.instance_setup.instance_config.config_manager.ConfigManager.SetOption')
+  @mock.patch('google_compute_engine.instance_setup.instance_config.config_manager.ConfigManager.__init__')
+  def testInstanceConfigError(self, mock_init, mock_set, mock_exists, mock_read):
+    mock_read.side_effect = instance_config.parser.ParsingError('Error')
+    mocks = mock.Mock()
+    mocks.attach_mock(mock_init, 'init')
+    mocks.attach_mock(mock_set, 'set')
+    mocks.attach_mock(mock_read, 'read')
+    mocks.attach_mock(mock_exists, 'exists')
+    mocks.attach_mock(self.mock_logger, 'logger')
+    mock_exists.return_value = True
+
+    instance_config.InstanceConfig(
+        logger=self.mock_logger, instance_config_metadata='Error')
+    expected_calls = [
+        mock.call.init(
+            config_file='template', config_header='/tmp/test.py template'),
+        mock.call.logger.error(mock.ANY, mock.ANY),
+        mock.call.exists('config'),
+        mock.call.read('config'),
+        mock.call.logger.error(mock.ANY, mock.ANY),
+        mock.call.exists('distro'),
+        mock.call.read('distro'),
+        mock.call.logger.error(mock.ANY, mock.ANY),
+        mock.call.set('first', 'a', 'false', overwrite=False),
+        mock.call.set('second', 'b', 'true', overwrite=False),
+        mock.call.set('third', 'c', '1', overwrite=False),
+        mock.call.set('third', 'd', '2', overwrite=False),
+        mock.call.set('third', 'e', '3', overwrite=False)
+    ]
+    self.assertEqual(mocks.mock_calls, expected_calls)
 
   @mock.patch('google_compute_engine.instance_setup.instance_config.config_manager.ConfigManager.WriteConfig')
   def testWriteConfig(self, mock_write):
