86868d458167b60d31aa7888331efcd8a43aaf7b
Zach Marano <zmarano@google.com>
2018-02-27 13:44:05 -0800

    Bump OS Login package version. (#564)

--- google_compute_engine_oslogin/packaging/setup_rpm.sh
+++ google_compute_engine_oslogin/packaging/setup_rpm.sh
@@ -20,7 +20,7 @@
 
 # Run from the top of the source directory.
 NAME="google-compute-engine-oslogin"
-VERSION="1.1.4"
+VERSION="1.1.5"
 
 working_dir=${PWD}
 rpm_working_dir=/tmp/rpmpackage/${NAME}-${VERSION}
