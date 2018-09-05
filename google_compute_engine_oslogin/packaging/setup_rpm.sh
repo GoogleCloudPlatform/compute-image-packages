ab79ceb03f3fc3f72a956bb02cd5344f567f9d6a
Zach Marano <zmarano@google.com>
2018-09-05 14:38:15 -0700

    fix

--- google_compute_engine_oslogin/packaging/setup_rpm.sh
+++ google_compute_engine_oslogin/packaging/setup_rpm.sh
@@ -20,7 +20,7 @@
 
 # Run from the top of the source directory.
 NAME="google-compute-engine-oslogin"
-VERSION="1.4.0"
+VERSION="1.3.1"
 
 working_dir=${PWD}
 rpm_working_dir=/tmp/rpmpackage/${NAME}-${VERSION}
