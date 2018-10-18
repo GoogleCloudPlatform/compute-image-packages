00650ab41e518be9cdee8520529214f7f392b70d
Lucas Kanashiro <lucas.kanashiro@collabora.com>
2018-10-18 18:12:29 -0300

    oslogin: fix utils/oslogin_utils.cc build in FreeBSD
    In FreeBSD boost/regex.hpp is not available and since clang
    is the default compiler we do not include it when __clang__
    is defined.

--- packages/google-compute-engine-oslogin/utils/oslogin_utils.cc
+++ packages/google-compute-engine-oslogin/utils/oslogin_utils.cc
@@ -23,8 +23,7 @@
 #include <iostream>
 #include <sstream>
 
-#ifdef __GNUC__
-#if __GNUC__ > 4 || \
+#if defined(__clang__) || __GNUC__ > 4 || \
   (__GNUC__ == 4 && (__GNUC_MINOR__ > 9 || \
                      (__GNUC_MINOR__ == 9 && \
                       __GNUC_PATCHLEVEL__ > 0)))
@@ -33,7 +32,6 @@
 #else
 #include <boost/regex.hpp>
 #define Regex boost
-#endif
 #endif
 
 #include "oslogin_utils.h"
