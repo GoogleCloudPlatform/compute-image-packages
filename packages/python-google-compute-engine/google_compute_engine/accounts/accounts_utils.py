5c4c862701a3a47be819a7ce7b41eebf74587033
Chris Debenham <chris@adebenham.com>
2019-04-29 14:51:35 +1000

    Get rid of extra check in hopes of testing passing

--- packages/python-google-compute-engine/google_compute_engine/accounts/accounts_utils.py
+++ packages/python-google-compute-engine/google_compute_engine/accounts/accounts_utils.py
@@ -98,12 +98,11 @@ class AccountsUtils(object):
       except subprocess.CalledProcessError as e:
         self.logger.warning('Could not create the sudoers group. %s.', str(e))
 
-    if os.path.exists(self.google_sudoers_file):
-      try:
-        if os.path.getsize(self.google_sudoers_file) == 0:
-          os.remove(self.google_sudoers_file)
-      except OSError as e:
-        pass
+    try:
+      if os.path.getsize(self.google_sudoers_file) == 0:
+        os.remove(self.google_sudoers_file)
+    except OSError as e:
+      pass
 
     if not os.path.exists(self.google_sudoers_file):
       try:
