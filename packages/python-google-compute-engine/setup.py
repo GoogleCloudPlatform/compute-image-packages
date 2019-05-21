b144e6360aaa09213ac79b42be872d38000d5625
Zach Marano <zmarano@google.com>
2019-05-21 13:28:27 -0700

    Bump versions to 2.8.15.

--- packages/python-google-compute-engine/setup.py
+++ packages/python-google-compute-engine/setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
     name='google-compute-engine',
     packages=setuptools.find_packages(),
     url='https://github.com/GoogleCloudPlatform/compute-image-packages',
-    version='2.8.13',
+    version='2.8.15',
     # Entry points create scripts in /usr/bin that call a function.
     entry_points={
         'console_scripts': [
