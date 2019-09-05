76af84fe0b08586defde3d16b747d460e91085d5
Liam Hopkins <liamh@google.com>
2019-09-05 11:11:31 -0700

    bump versions to 20190905

--- packages/python-google-compute-engine/packaging/setup_deb.sh
+++ packages/python-google-compute-engine/packaging/setup_deb.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 NAME="python-google-compute-engine"
-VERSION="20190801.00"
+VERSION="20190905.00"
 
 working_dir=${PWD}
 if [[ $(basename "$working_dir") != $NAME ]]; then
