a935dfe7dd0e0f779bcce915cd1f108e20cae53f
Liam Hopkins <liamh@google.com>
2019-09-30 11:40:32 -0700

    small fixes

--- packages/python-google-compute-engine/google_compute_engine/metadata_scripts/script_retriever.py
+++ packages/python-google-compute-engine/google_compute_engine/metadata_scripts/script_retriever.py
@@ -216,7 +216,7 @@ class ScriptRetriever(object):
       self.logger.info('Found %s in metadata.', metadata_key)
       with tempfile.NamedTemporaryFile(
           mode='w', dir=dest_dir, delete=False) as dest:
-        dest.write(metadata_value.lstrip())
+        dest.write(str(metadata_value.lstrip().encode('utf-8')))
         script_dict[metadata_key] = dest.name
 
     metadata_key = '%s-script-url' % self.script_type
