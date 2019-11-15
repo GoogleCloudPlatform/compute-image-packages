80b5b57a6150df008808b7100c50e924638858ed
Zach Marano <zmarano@google.com>
2019-11-15 14:04:33 -0800

    Up python package version.

--- packages/python-google-compute-engine/setup.py
+++ packages/python-google-compute-engine/setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
     name='google-compute-engine',
     packages=setuptools.find_packages(),
     url='https://github.com/GoogleCloudPlatform/compute-image-packages',
-    version='20191112.0',
+    version='20191115.0',
     # Entry points create scripts in /usr/bin that call a function.
     entry_points={
         'console_scripts': [
